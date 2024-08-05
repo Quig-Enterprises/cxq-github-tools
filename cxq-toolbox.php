0255a01a654be57effef0f785fe35db6db795f41
Crag-Monkey <brandon@quigs.com>
2024-08-05 14:41:15 -0500

    Update cxq-toolbox.php

--- cxq-toolbox.php
+++ cxq-toolbox.php
@@ -1,1 +1,23 @@
-testing
+<?php
+if( !defined( 'ABSPATH' ) ) {
+    header( 'Status: 403 Forbidden' );
+    header( 'HTTP/1.1 403 Forbidden' );
+    exit;
+}
+/*
+Plugin Name:       CxQ Toolbox
+Description:       A library of handy functions to simplify programming.
+Version:           1.0.0
+Author:            CxQ
+Plugin URI:        https://quigs.com
+Author URI:        https://quigs.com
+Update URI:        https://quigs.com/plugins/
+License:           GPL2
+License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+Requires PHP:      7.3
+Tested To:         6.6
+Requires at least: 5.8.3
+*/
+
+final class CxQ_Toolbox{
+}
